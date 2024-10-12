6942d4e1dcbc52e8a0ea471c3fd5d08261068023
AffanSurya <affanqwe04@gmail.com>
2024-10-12 20:57:15 +0700

    new file:   lib/core/configs/theme/app_colors.dart 	new file:   lib/core/configs/theme/app_theme.dart

---
+++ android/app/src/main/kotlin/com/example/zerofy_musicapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.zerofy_musicapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/// Flutter code sample for [MenuAnchor].
+
+void main() => runApp(const MyApp());
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+}
+
+
