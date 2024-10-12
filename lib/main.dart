b3e545f1cfc35f680a9d878b38879309a45463c8
AffanSurya <affanqwe04@gmail.com>
2024-10-12 22:15:21 +0700

    Refactor app theme configuration and add custom font support

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:zerofy_musicapp/core/configs/theme/app_theme.dart';
 
 /// Flutter code sample for [MenuAnchor].
 
 void main() => runApp(const MyApp());
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.lighTheme,
+      home: Container(),
+    );
+  }
 }
-
-
