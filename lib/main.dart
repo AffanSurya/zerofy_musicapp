afd79eb9063f95ecbd642e5e2ee17a4379d296b0
AffanSurya <affanqwe04@gmail.com>
2024-10-21 20:59:10 +0700

    Refactor app theme configuration and add custom font support

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,40 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:zerofy_musicapp/core/configs/theme/app_theme.dart';
+import 'package:zerofy_musicapp/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:zerofy_musicapp/presentation/splash/pages/splash.dart';
 
 /// Flutter code sample for [MenuAnchor].
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getApplicationDocumentsDirectory(),
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme.lighTheme,
-      debugShowCheckedModeBanner: false,
-      home: const SplashPage(),
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (_) => ThemeCubit())],
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, mode) => MaterialApp(
+          theme: AppTheme.lighTheme,
+          darkTheme: AppTheme.darkTheme,
+          themeMode: mode,
+          debugShowCheckedModeBanner: false,
+          home: const SplashPage(),
+        ),
+      ),
     );
   }
 }
